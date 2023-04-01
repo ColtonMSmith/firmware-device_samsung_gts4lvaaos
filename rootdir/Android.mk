104dcbfb0f23d1b684e7ad7917afb7e8bc599eaa
Colton <wyoming454@gmail.com>
2023-04-01 17:48:37 -0600

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,27 +1,48 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 LOCAL_PATH := $(call my-dir)
 
-# Device init scripts
-
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.gts4lv.rc
+LOCAL_MODULE       := init.mt8163.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.gts4lv.rc
+LOCAL_SRC_FILES    := etc/init.mt8163.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.vendor.rilchip.rc
+LOCAL_MODULE       := init.mt8163.usb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.vendor.rilchip.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+LOCAL_SRC_FILES    := etc/init.mt8163.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.vendor.rilcommon.rc
+LOCAL_MODULE       := fstab.mt8163
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.vendor.rilcommon.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+LOCAL_SRC_FILES    := etc/fstab.mt8163
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.mt8163_first_stage
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_STEM  := fstab.mt8163
+LOCAL_SRC_FILES    := etc/fstab.mt8163
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.mt8163.rc
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/ueventd.mt8163.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
