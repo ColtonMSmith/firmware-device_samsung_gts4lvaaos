dc07b91049f73b5dc59cea8e110aba640b4cef56
Colton <wyoming454@gmail.com>
2023-04-01 17:32:53 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,43 +1,165 @@
 #
-# Copyright (C) 2020-2021 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2023 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: Apache-2.0
 #
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# inherit from common gts4lv-common
--include device/samsung/gts4lv-common/BoardConfigCommon.mk
+DEVICE_PATH := device/amazon/karnak
 
-DEVICE_PATH := device/samsung/gts4lv
+# Platform
+TARGET_BOARD_PLATFORM := mt8163
+TARGET_BOARD_PLATFORM_GPU := mali-720mp2
+TARGET_BOOTLOADER_BOARD_NAME := karnak
+BOARD_HAS_MTK_HARDWARE := true
 
-# Assert
-TARGET_OTA_ASSERT_DEVICE := gts4lv
+# Bootloader
+TARGET_NO_BOOTLOADER := true
 
-# Board
-TARGET_BOARD_NAME := SRPRL03A001
+# Architecture
+TARGET_ARCH := arm
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a7
+TARGET_CPU_VARIANT:= cortex-a7
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 
-# HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+# OTA Assert
+TARGET_OTA_ASSERT_DEVICE:= karnak
 
-# Kernel
-TARGET_KERNEL_CONFIG := gts4lv_defconfig
-BOARD_MKBOOTIMG_ARGS += --board $(TARGET_BOARD_NAME)
+# Kernel Config
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/amazon/karnak
+TARGET_KERNEL_CONFIG := lineageos_karnak_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := lineageos_karnak_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/linaro/linux-x86/aarch64/aarch64-linux-gnu/bin/aarch64-linux-gnu-
+TARGET_KERNEL_CLANG_COMPILE := false
+TARGET_KERNEL_OPTIONAL_LD := true
+
+# Boot.img
+BOARD_RAMDISK_USE_LZ4     := true
+BOARD_KERNEL_BASE         := 0x40080000
+BOARD_KERNEL_PAGESIZE     := 2048
+BOARD_KERNEL_OFFSET       := 0x00000000
+BOARD_RAMDISK_OFFSET      := 0x03400000
+BOARD_KERNEL_TAGS_OFFSET  := 0x07f80000
+BOARD_SECOND_OFFSET       := 0x00e80000
+BOARD_MKBOOTIMG_ARGS := \
+    --kernel_offset $(BOARD_KERNEL_OFFSET) \
+    --ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
+    --second_offset $(BOARD_SECOND_OFFSET)   \
+    --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
+
+# Build Vendor Image
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_USES_VENDORIMAGE := true
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Binder API version
+TARGET_USES_64_BIT_BINDER := true
+
+# WIFI
+BOARD_WLAN_DEVICE := MediaTek
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
+WIFI_DRIVER_FW_PATH_PARAM := "/dev/wmtWifi"
+WIFI_DRIVER_FW_PATH_STA:=STA
+WIFI_DRIVER_FW_PATH_AP:=AP
+WIFI_DRIVER_FW_PATH_P2P:=P2P
+WIFI_DRIVER_STATE_CTRL_PARAM := /dev/wmtWifi
+WIFI_DRIVER_STATE_ON := 1
+WIFI_DRIVER_STATE_OFF := 0
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
+
+# BT
+BOARD_HAS_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Filesystem
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3253731328
+BOARD_CACHEIMAGE_PARTITION_SIZE := 524288000
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_PARTITION_SIZE := 235929600
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11633933824
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
+BLOCK_BASED_OTA := true
+
+# Seccomp filters
+BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/configs/seccomp
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
+
+# Software Gatekeeper
+BOARD_USE_SOFT_GATEKEEPER := true
+
+# Manifest
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/device_framework_compatibility_matrix.xml
+
+# Display
+TARGET_SCREEN_DENSITY := 213
+
+# Audio
+USE_XML_AUDIO_POLICY_CONF := 1
+USE_CUSTOM_AUDIO_POLICY := 1
+
+# Exclude AudioFx
+TARGET_EXCLUDES_AUDIOFX := true
+
+# Graphics
+TARGET_USES_HWC2 := true
+
+# Treble
+SELINUX_IGNORE_NEVERALLOWS := true
+BUILD_BROKEN_PREBUILT_ELF_FILES := true
+BUILD_BROKEN_VINTF_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# LMKD stats logging
+TARGET_LMKD_STATS_LOG := true
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2020-08-03
+
+# TWRP
+ifneq (,$(strip $(wildcard bootable/recovery-twrp/twrp.cpp)))
+RECOVERY_VARIANT := twrp
+WITH_TWRP := true
+endif
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt8163
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone2/temp
+DEVICE_RESOLUTION := 800x1280
+TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_USE_SYSTEM_VOLD := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_DEFAULT_BRIGHTNESS := 128
+TW_AMONET := true
+TW_DEFAULT_BACKUP_LIST := "/system_image;/vendor_image;/data;/boot;"
 
 # Properties
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
-# RIL
-BOARD_PROVIDES_LIBRIL := true
-ENABLE_VENDOR_RIL_SERVICE := true
-
-# inherit from the proprietary version
-include vendor/samsung/gts4lv/BoardConfigVendor.mk
+# Inherit from the proprietary version
+include vendor/amazon/mt8163/BoardConfigVendor.mk
+include vendor/amazon/karnak/BoardConfigVendor.mk
